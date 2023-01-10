484f1fcc0d8b901a3642cbb7e39be75a2b9fceeb
Nathaniel <natharevolution@gmail.com>
2023-01-10 15:30:18 -0400

    agregando funcionalida para sacar informacion de A.P.I con un respectivo helper

--- proyecto-blog/src/components/layout/Header.jsx
+++ proyecto-blog/src/components/layout/Header.jsx
@@ -4,9 +4,9 @@ export const Header = () => {
   return (
     //  Cabecera
     <header className="header">
-      <div className="logo">
+      {/* <div className="logo">
         <div className="play"></div>
-      </div>
+      </div> */}
       <h1>Blog de React Js</h1>
     </header>
   );
