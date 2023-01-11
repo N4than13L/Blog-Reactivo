b0fcc61e5969891d45b803ed615f772ff09c4182
Nathaniel <natharevolution@gmail.com>
2023-01-11 16:29:55 -0400

    agregando hook para formulario, refactorizando el codigo en apartado de articulos y creando su respectivo helper

--- proyecto-blog/src/components/pages/Articulos.jsx
+++ proyecto-blog/src/components/pages/Articulos.jsx
@@ -2,10 +2,13 @@ import React from "react";
 // 1. se importa los hooks useState y UseEffect.
 import { useState, useEffect } from "react";
 import { global } from "../../helpers/global";
+import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
 
 export const Articulos = () => {
   // 2. Se crea el stado.
   const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
   // 3. se instancia el useEffect.
   useEffect(() => {
@@ -15,40 +18,23 @@ export const Articulos = () => {
   // 4. hacer la funcion correspondiente para conseguir los datos del api.
   const conseguir_articulos = async () => {
     // obtener la url.
-    const url = global.url + "articulos";
 
-    // hacer una variable para el await.
-    let peticion = await fetch(url, {
-      method: "GET",
-    });
-
-    // darle formato JSON a la variable de la peticion.
-    let datos = await peticion.json();
+    // console.log(await Peticion(global.url + "articulos", "GET"));
+    const { datos, cargando } = await Peticion(global.url + "articulos", "GET");
 
     if (datos.status === "success") {
       setArticulos(datos.articulos);
     }
+
+    setCargando(false);
   };
 
   return (
     <>
       {/* hacer la condicion ternaria (o de una sola linea) para recoger la informacion si la tiene el api y si no dar un mensaje de error. */}
+      {cargando ? "cargando..." : ""}
       {articulos.length >= 1 ? (
-        articulos.map((articulo) => {
-          return (
-            <article key={articulo._id} className="articulo-item">
-              <div className="mascara">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/640px-Unofficial_JavaScript_logo_2.svg.png" />
-              </div>
-              <div className="datos">
-                <h3 className="title">{articulo.titulo}</h3>
-                <p className="description">{articulo.contenido}</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-              </div>
-            </article>
-          );
-        })
+        <Listado articulos={articulos} setArticulos={setArticulos} />
       ) : (
         <h1>Errror 404 no hay articulos.</h1>
       )}
--- proyecto-blog/src/components/pages/Crear.jsx
+++ proyecto-blog/src/components/pages/Crear.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export const Crear = () => {
-  return <div>Crear</div>;
+  return (
+    <div className="jumbo">
+      <h1>Crear articulos</h1>
+      <h3>Formulario para crear articulos.</h3>
+      {/* Montar el formulario */}
+      <form className="formulario">
+        <div className="form-group">
+          <label htmlFor="titulo">titulo</label>
+          <input type="text" name="titulo" placeholder="Titulo" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="contenido">contenido</label>
+          <textarea name="contenido" placeholder="Contenido" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="imagen">imagen</label>
+          <input type="file" name="file0" id="imagen" />
+        </div>
+
+        <input type="submit" value="publicar" className="btn btn-success" />
+      </form>
+    </div>
+  );
 };
